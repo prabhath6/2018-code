59fe8d8e757dd7b924eab19ead509c9356b27824
Prabhath Kiran <prabhathkiran20@gmail.com>
2018-01-17 23:24:38 -0600

    Parse the apis to get required data

--- Weather_App/index.js
+++ Weather_App/index.js
@@ -27,14 +27,28 @@ function getCoordinates() {
 
 function getTemp(position) {
 
+    var weather = document.getElementById("weather");
+
     var request = $.ajax({
         url: `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
         method: "GET",
         dataType: "JSON"
       });
 
-      request.done(function( msg ) {
-          console.log(msg);
+      request.done(function(msg) {
+
+        console.log(JSON.stringify(msg, null, '\t'));
+          var usefulData = {};
+          usefulData["state"] = msg.name;
+          usefulData["country"] = msg.sys.country;
+          usefulData["weather"] = msg.weather[0].main;
+          usefulData["weather-description"] = msg.weather[0].description;
+          usefulData["temp"] = msg.main.temp;
+          usefulData["pressure"] = msg.main.pressure;
+          usefulData["humidity"] = msg.main.humidity;
+          usefulData["wind"] = msg.wind.speed;
+
+          weather.innerText = JSON.stringify(usefulData, null, '\t');
       });
        
       request.fail(function( jqXHR, textStatus ) {
