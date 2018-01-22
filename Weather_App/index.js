ac9ba94933f414f4ff1465abc23717a65c7c178b
Prabhath Kiran <prabhathkiran20@gmail.com>
2018-01-21 19:40:29 -0600

    Add basic styling and temp conversions

--- Weather_App/index.js
+++ Weather_App/index.js
@@ -4,10 +4,10 @@ function getCoordinates() {
     var lat = document.getElementById("lat");
     var log = document.getElementById("log");
 
-    if (!navigator.geolocation){
+    if (!navigator.geolocation) {
         lat.innerText = "Geolocation is not supported by your browser";
         return;
-      }
+    }
 
     function success(position) {
         getTemp(position);
@@ -29,43 +29,46 @@ function getTemp(position) {
         url: `https://fcc-weather-api.glitch.me/api/current?lat=${position.coords.latitude}&lon=${position.coords.longitude}`,
         method: "GET",
         dataType: "JSON"
-      });
+    });
 
-      request.done(function(msg) {
+    request.done(function (msg) {
+        var humidity = document.getElementById("weather-humidity");
+        var temp_min = document.getElementById("weather-min");
+        var temp_max = document.getElementById("weather-max");
+        var wind_speed = document.getElementById("weather-speed");
 
-          var usefulData = {};
-          usefulData["state"] = msg.name;
-          usefulData["country"] = msg.sys.country;
-          usefulData["weather"] = msg.weather[0].main;
-          usefulData["weather-id"] = msg.weather[0].id; 
-          usefulData["weather-description"] = msg.weather[0].description;
-          usefulData["temp"] = msg.main.temp;
-          usefulData["pressure"] = msg.main.pressure;
-          usefulData["humidity"] = msg.main.humidity;
-          usefulData["wind"] = msg.wind.speed;
+        var usefulData = {};
+        usefulData["state"] = msg.name;
+        usefulData["country"] = msg.sys.country;
+        usefulData["weather"] = msg.weather[0].main;
+        temp_min.innerText = msg.main.temp_max;
+        temp_max.innerText = msg.main.temp_min;
+        humidity.innerText = msg.main.humidity;
+        wind_speed.innerText = msg.wind.speed;
+        temp = msg.main.temp;
+        var iconCodeHtml = '<i class="wi wi-owm-' + msg.weather[0].id + '"></i>';
+        weatherIcon.innerHTML = iconCodeHtml;
+        weather.innerText = JSON.stringify(usefulData, null, '\t');
 
-          temp = usefulData["temp"];
+        var tempVal = document.getElementById("temp-val");
+        tempVal.innerText = temp;
+    });
 
-          var iconCodeHtml = '<i class="wi wi-owm-' + usefulData["weather-id"] + '"></i>';
-          weatherIcon.innerHTML = iconCodeHtml; 
-          weather.innerText = JSON.stringify(usefulData, null, '\t');
-
-          // Fah
-          var degrees = document.getElementById("weather-degree");
-          degrees.innerHTML = '<p id="temp-val">' + temp + '</p>' + '<i id="degree-icon" class="wi wi-celsius" onclick="convertTemp();"></i>';
-      });
-       
-      request.fail(function( jqXHR, textStatus ) {
-        alert( "Request failed: " + textStatus );
-      });
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
+    });
 }
 
 function convertTemp() {
     var getDegreeClass = document.getElementById("degree-icon").classList;
-    if(getDegreeClass[1].includes("celsius")) {
+    var getDegrees = document.getElementById("temp-val");
+    var getDegreesConverted = parseFloat(getDegrees.innerText);
+    if (getDegreeClass[1].includes("celsius")) {
+        getDegrees.innerText = ((getDegreesConverted * (9 / 5)) + 32).toFixed(2);
         getDegreeClass.remove("wi-celsius");
         getDegreeClass.add('wi-fahrenheit');
     } else {
+        getDegrees.innerText = ((getDegreesConverted - 32) * (5 / 9)).toFixed(2);
         getDegreeClass.remove("wi-fahrenheit");
         getDegreeClass.add('wi-celsius');
     }
